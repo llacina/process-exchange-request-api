88fdd6944abc2d19c70198761b1d88c979ffe80b
Lubos Lacina <lacom@email.cz>
2019-06-06 12:47:34 +0200

    Add status to message

---
+++ src/main/kotlin/com/wbtcb/process/exchange/request/RabbitMqConstants.kt
@@ -0,0 +1,11 @@
+package com.wbtcb.process.exchange.request
+
+class RabbitMqConstants {
+
+    companion object {
+
+        const val EXCHANGE_REQUEST = "exchange-request"
+
+        const val EXCHANGE_REQUEST_ROUTING_PREFIX_KEY = EXCHANGE_REQUEST
+    }
+}
